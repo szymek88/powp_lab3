76adbc6e73e02101d20c93acbba0ba9371ebdbb8
Szymon Papuga <szymek.p43@gmail.com>
2016-09-15 05:21:23 +0200

    Line Plotter Adapter dodany do TestPlotSoftPatterns

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
 import edu.iis.client.plottermagic.preset.FiguresJoe;
+import edu.iis.powp.adapter.LinePlotterAdapter;
 import edu.iis.powp.adapter.PlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
@@ -17,6 +18,7 @@ import edu.iis.powp.events.predefine.SelectChangeVisibleOptionListener;
 import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.LineFactory;
 
 
 public class TestPlotSoftPatterns
@@ -49,6 +51,9 @@ public class TestPlotSoftPatterns
 		IPlotter plotter = new PlotterAdapter();
 		context.addDriver("Plotter Adapter", plotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(plotter);
+		
+		IPlotter linePlotter = new LinePlotterAdapter(LineFactory.getSpecialLine());
+		context.addDriver("Line Plotter Adapter", linePlotter);
 
 		context.updateDriverInfo();
 	}
